6d4672efe7b5526d1ede8779366c8138e6b0d03c
Jason Li <jlee77711@outlook.com>
2023-03-30 20:55:48 +0000

    Name JL

---
+++ HackerRankExcercise.py
@@ -0,0 +1,9 @@
+#My name is Jason Li. 
+
+print("JJJJJJ L      ")
+print("  J    L      ")
+print("  J    L      ")
+print("  J    L      ")
+print("  J    L      ")
+print("J J    L      ")
+print("JJJ    LLLLLLL")
