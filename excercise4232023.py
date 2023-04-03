34241e08a0ab3248472c56b5bc5b3d325516595f
Jason Li <jlee77711@outlook.com>
2023-04-03 00:32:01 +0000

    multiplicationexcercise

---
+++ excercise4232023.py
@@ -0,0 +1,8 @@
+# Assign the value 2 to the variable x
+x = 5
+# Assign the value 3 to the variable y
+y = 3
+# Multiply x and y and store the result in the variable 'result'
+result = x * y
+# Print the value stored in 'result'
+print(result)
