1104a6af128a25f1a1adddf608b2883ddf5a09dd
Jason Li <jlee77711@outlook.com>
2023-04-08 17:09:06 +0000

    CalculateareaofRectangle

---
+++ CalculateareaofaTriangle.py
@@ -0,0 +1,9 @@
+# Input the length and width of the rectangle
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+# Calculate the area of the rectangle
+area = length * width
+
+# Print the result
+print("The area of the rectangle is:", area)
