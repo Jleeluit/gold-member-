7fec850ae9466633bdf3c91d6a04934006605aee
Jason Li <jlee77711@outlook.com>
2023-04-08 17:13:22 +0000

    calculateareoftriangle

--- CalculateareaofaTriangle.py
+++ CalculateareaofaTriangle.py
@@ -1,9 +1,9 @@
-# Input the length and width of the rectangle
-length = float(input("Enter the length of the rectangle: "))
-width = float(input("Enter the width of the rectangle: "))
+# Input the base and height of the triangle
+base = float(input("Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
 
-# Calculate the area of the rectangle
-area = length * width
+# Calculate the area of the triangle
+area = 0.5 * base * height
 
 # Print the result
-print("The area of the rectangle is:", area)
+print("The area of the triangle is:", area)
